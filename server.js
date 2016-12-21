2d58182f28295dad4f1da6ee8289c4649e0d8098
Pongsathorn C <bpkiller45@gmail.com>
2016-12-21 16:39:42 +0700

    Add POST for todos

--- server.js
+++ server.js
@@ -49,6 +49,18 @@ app.post('/todos/',function(req,res){
   //console.log(body);
   res.json(body);
 });
+//DELETE /todos/:id
+app.delete('/todos/:id',function(req,res){
+  var todoId = parseInt(req.params.id,10);
+  var matchedTodo = _.findWhere(todos,{id:todoId});
+  if(!matchedTodo) {
+    res.status(404).json({"error":"no todo with that id"});
+  } else {
+    todos = _.without(todos,matchedTodo);
+    res.status(200).send();
+  }
+});
+
 app.listen(PORT,function() {
   console.log('Express listening on port: ' + PORT);
 })
